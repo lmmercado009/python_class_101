a9f694273add8efa37a7fba9643b3371eb118a45
Luis Mercado <lmmercado009@gmail.com>
2024-09-22 12:33:14 -0500

    added first project

---
+++ 02-IDE_exercises/01-project/my_color.py
@@ -0,0 +1,7 @@
+# Author: Luis Mercado
+# Date: 09/22/2024
+# Description: Asks the user for their favorite color and then
+#              prints out a statement with the variable
+
+fave_color = input("Please enter your favorite color: ")
+print("Your favorite color is  " + fave_color + ".")
