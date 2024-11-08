005af749ce0a1b6089164aab09787ed2eccf21a1
Luis Mercado <lmmercado009@gmail.com>
2024-11-07 19:20:43 -0600

    Add 08a Project

---
+++ 02-IDE_exercises/08a-project/count_letters.py
@@ -0,0 +1,34 @@
+#Author: Luis Mercado
+#Date: 11/06/2024
+#Description: Function that takes a string argument and returns a dictionary that
+#             counts how many of each letter is in that string
+
+
+def count_letters(arg_string: str) -> dict[str, int]:
+    """
+    Takes a string as argument and returns a dictionary whose keys are each alphabet letter
+    present in the string and whose values are the count of each letter.
+    """
+
+    #create empty dictionary to which keys and values will be iteratively added
+    letter_count: dict[str, int] = {}
+
+    #iterate through characters (all made upper-case) of arg_string, checking whether is is alphabetic,
+    #if alphabetic and NOT YET IN letter_count, add key and value of 1
+    #if alphabetic and already in letter_count, increase value of that key by 1
+
+    letter: str
+
+    for letter in arg_string:
+        if "A" <= letter.upper() <= "Z":
+            if letter.upper() not in letter_count:
+                letter_count[letter.upper()] = 1
+            else:
+                letter_count[letter.upper()] += 1
+
+    return letter_count
+
+
+#test code
+test_string: str = "$ % G h g ytuCBA"
+print(count_letters(test_string))
